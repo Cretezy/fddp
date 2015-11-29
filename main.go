224d2cb59c7f8408dc3589ec466d8952aade9915
Charles-William Crete <git@charles-william.me>
2015-11-29 12:00:01 -0500

    Added extra line before time output

--- main.go
+++ main.go
@@ -12,9 +12,10 @@ func main() {
 	start := time.Now()
 
 	app := cli.NewApp()
+
 	app.Name = "fddp"
 	app.Usage = "Facebook Downloaded Data Processor"
-	app.Description = "processes Facebook messages"
+	app.Description = "processes Facebook messages (format conversion, research, etc)"
 
 	app.Commands = Commands()
 
