af2265227178abf1aa213402f846c68cf684ad1b
Charles-William Crete <git@charles-william.me>
2015-12-04 14:13:27 -0500

    Added some support for gin?

--- commands.go
+++ commands.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/CraftThatBlock/fddp/Godeps/_workspace/src/github.com/codegangsta/cli"
-	"github.com/CraftThatBlock/fddp/server"
 )
 
 func Commands() []cli.Command {
 	return []cli.Command{
 		ConvertCommand(),
-		server.ServerCommand(),
+		ServerCommand(),
 		CountCommand(),
 		CompareCommand(),
 		ListCommand(),
---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/CraftThatBlock/fddp/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/CraftThatBlock/fddp/Godeps/_workspace/src/github.com/go-martini/martini"
+)
+
+func ServerCommand() cli.Command {
+	return cli.Command{
+		Name:        "server",
+		Usage:       "run web app",
+		Description: "Use enviroment variable PORT to set which port to listen to.",
+		ArgsUsage:   "",
+		Action:      ServerAction,
+		Flags:       []cli.Flag{},
+	}
+}
+
+func ServerAction(c *cli.Context) {
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello  world!"
+	})
+	m.Run()
+}
