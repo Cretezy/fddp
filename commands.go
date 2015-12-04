60c6ea82b6ba296c1a78ca485d6033801ba3738b
Charles-William Crete <git@charles-william.me>
2015-12-03 23:44:08 -0500

    Add basic server command

--- commands.go
+++ commands.go
@@ -1,10 +1,14 @@
 package main
 
-import "github.com/CraftThatBlock/fddp/Godeps/_workspace/src/github.com/codegangsta/cli"
+import (
+	"github.com/CraftThatBlock/fddp/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/CraftThatBlock/fddp/server"
+)
 
 func Commands() []cli.Command {
 	return []cli.Command{
 		ConvertCommand(),
+		server.ServerCommand(),
 		CountCommand(),
 		CompareCommand(),
 		ListCommand(),
