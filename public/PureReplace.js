e0f96cfd7b5e3f4e94c85aeb5565ae89fae104ef
Charles-William Crete <git@charles-william.me>
2015-12-08 23:01:48 -0500

    Fix bugs + new version of PureReplace

--- public/PureReplace.js
+++ public/PureReplace.js
@@ -1,121 +1,166 @@
-var prefix = "pr-";
-var pageHolderTag = prefix + "data";
-var pagesDirectory = "pages";
+var PureReplace = {
+    // // // // // // // // // // // // //
+    //  You may modify these options.   //
+    // // // // // // // // // // // // //
 
-var currentPage;
-var callbacks = [];
-/**
- * Start up
- */
-function startPureReplace(){
-    hashCheck(true); // First load
+    pageHolderTag: "pr-data",
+    pagesDirectory: "pages",
 
-    window.onhashchange = function () {
-        hashCheck(false); // Any changes after
-    }
-}
+    // // // // // // // // // // // // // // // // // // // // // // // //
+    //  No not edit past this point unless you know what you are doing.  //
+    // // // // // // // // // // // // // // // // // // // // // // // //
 
-function hashCheck(isFirstLoad) {
-    // Get hash (page name)
-    var hash = window.location.hash.substr(1);
-    // Scroll later if first load
-    if (isFirstLoad || !scrollToHash(hash)) {
+    /**
+     * Variables
+     */
+    currentPage: "",
+    pageCallbacks: [], globalCallbacks: [],
+
+
+    /**
+     * Start up
+     */
+    start: function () {
+        PureReplace.hashCheck(true); // First load
+
+        window.onhashchange = function () {
+            PureReplace.hashCheck(false); // Any changes after
+        }
+    },
+
+    /**
+     * Runs on each changes of hash -> changes page
+     *
+     * @param isFirstLoad First time loading
+     */
+    hashCheck: function (isFirstLoad) {
+        // Get hash (page name)
+        var hash = window.location.hash.substr(1);
+        // Scroll later if first load
         // Default to "home"
-        if (hash == "")
-            switchPage('home', function () {
-                if (isFirstLoad)
-                    scrollToHash(hash);
-            });
-        else
-            switchPage(hash.split("#")[0], function () {
-                if (isFirstLoad)
-                    scrollToHash(hash);
-            });
-    }
-}
+        if (hash == "") {
 
-/**
- * Scroll to element based on hash
- * Format: page#element
- *
- * @param hash
- * @returns {boolean} Has scrolled (has an element)
- */
-function scrollToHash(hash) {
-    if (hash.indexOf("#") > -1) {
-        var element = hash.split("#")[1];
-        if (element == "top") {
-            scrollToTop();
-            return false;
+            PureReplace.switchPage('home', null);
         } else {
-            document.getElementById(element).scrollIntoView(true);
-            window.location.hash = "#" + hash;
-            return true
+            PureReplace.switchPage(hash.split("#")[0], function () {
+                PureReplace.scrollToHash(hash);
+            });
         }
-    } else {
-        scrollToTop();
+
+    },
+
+    /**
+     * Scroll to element based on hash
+     * Format: page#element
+     *
+     * @param hash
+     * @returns {boolean} Has scrolled (has an element)
+     */
+    scrollToHash: function (hash) {
+        if (hash.indexOf("#") > -1) { // Has a sub-hash (scroll-to)
+            var element = hash.split("#")[1]; // Get sub-hash
+            if (element == "top") { // Scroll to top if "top"
+                PureReplace.scrollToTop();
+                return false;
+            } else { // Scroll to element
+                document.getElementById(element).scrollIntoView(true);
+                window.location.hash = "#" + hash;
+                return true;
+            }
+        } else {
+            PureReplace.scrollToTop();
+            return false;
+        }
+    },
+
+    /**
+     * Scroll to top of the page
+     */
+    scrollToTop: function () {
+        window.scrollTo(0, 0);
+    },
+
+    /**
+     * Get content of url
+     *
+     * @param url URL
+     * @param callback
+     * @returns {string} Content
+     */
+    httpGet: function (url, callback) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                callback(xmlHttp.responseText);
+            }
+        };
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send();
+    },
+
+    /**
+     * Fix links of current page to links to replace
+     */
+    updateLinks: function () {
+        // Find all links
+        var links = document.getElementsByTagName('a');
+        for (var i = 0; i < links.length; i++) {
+            var link = links[i];
+            // Check if link starts with ## tag
+            if (link.href.split("#").length > 2) {
+                // Make it link to self-page plus element
+                link.href = "#" + window.location.hash.split("#")[1] + "#" + link.href.split("#")[2];
+            }
+        }
+    },
+
+    /**
+     * Replace to page
+     *
+     * @param newPage New page
+     * @param callback Callback
+     */
+    switchPage: function (newPage, callback) {
+        if (PureReplace.currentPage == newPage) { // Don't double switch
+            if (callback != null)
+                callback(false); // Run callback once done
+        } else {
+            PureReplace.currentPage = newPage;
+            PureReplace.httpGet(PureReplace.pagesDirectory + '/' + PureReplace.currentPage + '.html', function (body) {
+                // Replace with new page
+                document.getElementById(PureReplace.pageHolderTag).innerHTML = body;
+                PureReplace.updateLinks(); // Fix scroll-to links
+                window.location.hash = "#" + newPage; // Set new url hash
+                PureReplace.scrollToTop(); // Go to top
+                PureReplace.globalCallbacks.forEach(function (globalCallback) {
+                    globalCallback(); // Run all global page load callbacks
+                });
+                PureReplace.pageCallbacks.forEach(function (pageCallback) {
+                    if (pageCallback.page == PureReplace.currentPage)
+                        pageCallback.callback(); // Run page-specific load callback
+                });
+                if (callback != null)
+                    callback(true); // Run callback once done
+            });
+        }
+    },
+
+    /**
+     * Add a callback to be ran after all page load
+     *
+     * @param callback Callback
+     */
+    addGlobalLoadCallback: function (callback) {
+        PureReplace.globalCallbacks.push(callback)
+    },
+
+    /**
+     * Add a callback to be ran after a specific page load
+     *
+     * @param page Page
+     * @param callback Callback
+     */
+    addPageLoadCallback: function (page, callback) {
+        PureReplace.pageCallbacks.push({page: page, callback: callback})
     }
-    return false
-}
-
-function scrollToTop() {
-    window.scrollTo(0, 0);
-}
-
-/**
- * Get content of url
- *
- * @param url URL
- * @param callback
- * @returns {string} Content
- */
-function httpGet(url, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            callback(xmlHttp.responseText)
-        }
-    };
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send();
-}
-
-/**
- * Fix links of current page to links to replace
- */
-function updateLinks() {
-    // Find all links
-    var links = document.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        // Check if link starts with ## tag
-        if (link.href.split("#").length > 2) {
-            // Make it link to self-page plus element
-            link.href = "#" + window.location.hash.split("#")[1] + "#" + link.href.split("#")[2];
-        }
-    }
-}
-
-/**
- * Replace to page
- *
- * @param newPage Page to replace to
- * @param callback
- */
-function switchPage(newPage, callback) {
-    currentPage = newPage;
-    httpGet(pagesDirectory + '/' + newPage + '.html', function (body) {
-        document.getElementById(pageHolderTag).innerHTML = body;
-        updateLinks();
-        window.location.hash = "#" + newPage;
-        scrollToTop();
-        callbacks.forEach(function(pageLoadCallback){
-            pageLoadCallback();
-        });
-        callback();
-    });
-}
-
-function onPageLoad(callback){
-    callbacks.push(callback)
-}
+};
--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    onPageLoad(function() {
+$(document).ready(function () {
+    PureReplace.addPageLoadCallback("convert", function () {
         $("#convertForm").submit(function (e) {
             e.preventDefault();
             if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
@@ -11,15 +11,12 @@ $(document).ready(function(){
             reader.onload = function () {
                 $.ajax({
                     url: form.attr("action"),
-                    data: { "messages": window.atob(reader.result.split(",")[1])},
+                    data: {"messages": window.atob(reader.result.split(",")[1])},
                     cache: false,
                     type: form.attr("method"),
                     success: function (data) {
-                        switchPage("show", function () {
-                            setTimeout(function(){
-                                $("#whoami").html(data.whoami);
-                            }, 20);
-                            // bug?? have to wait 20 ms before setting or else resets, wat?
+                        PureReplace.switchPage("show", function () {
+                            $("#whoami").html(data.whoami);
                         });
                     }
                 });
@@ -27,6 +24,6 @@ $(document).ready(function(){
             reader.readAsDataURL(form.find("#file")[0].files[0]);
         });
     });
-    startPureReplace();
+    PureReplace.start();
 });
 
